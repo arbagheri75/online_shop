8b123d8816cc4dce025cb82170283528d84c8134
arbagheri75 <ar.bagheri0075@gmail.com>
2023-01-23 01:00:26 +0330

    convert User to CustomUser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+
+    list_display = ('username', 'is_staff', 'is_active')
+    ordering = ('date_joined',)
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Startapp
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# get_user_mode Config
+AUTH_USER_MODEL = 'accounts.CustomUser'
